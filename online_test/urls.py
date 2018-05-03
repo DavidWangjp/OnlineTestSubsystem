2df54011cf416d950879333a15fa05c9c12741a8
DavidWang <a394027456@163.com>
2018-05-03 21:29:31 +0800

    Online test page.

---
+++ online_test/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'online_test'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('test/<int:pk>/', views.TestDetail.as_view(), name='test_detail')
+]
