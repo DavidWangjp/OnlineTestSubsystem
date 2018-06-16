8d5b8e5619bb12549a716efb2fddf6dd4733d87c
DavidWang <a394027456@163.com>
2018-06-16 14:21:59 +0800

    Add statistics.

--- online_test/models.py
+++ online_test/models.py
@@ -23,6 +23,13 @@ class Student(models.Model):
         return self.name
 
 
+class Subject(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Chapter(models.Model):
     chapter = models.CharField(max_length=10)
 
@@ -51,6 +58,7 @@ class ChoiceQuestion(models.Model):
 
     creator = models.ForeignKey(Teacher, on_delete=models.SET_NULL, blank=True, null=True)
 
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, blank=True, null=True)
     chapter = models.ForeignKey(Chapter, on_delete=models.SET_NULL, blank=True, null=True)
     knowledge_point = models.ForeignKey(KnowledgePoint, on_delete=models.SET_NULL, blank=True, null=True)
 
@@ -70,6 +78,7 @@ class TrueOrFalseQuestion(models.Model):
 
     creator = models.ForeignKey(Teacher, on_delete=models.SET_NULL, blank=True, null=True)
 
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, blank=True, null=True)
     chapter = models.ForeignKey(Chapter, on_delete=models.SET_NULL, blank=True, null=True)
     knowledge_point = models.ForeignKey(KnowledgePoint, on_delete=models.SET_NULL, blank=True, null=True)
 
@@ -87,6 +96,8 @@ class Test(models.Model):
     true_or_false_questions = models.ManyToManyField(TrueOrFalseQuestion, null=True, blank=True)
 
     creator = models.ForeignKey(Teacher, on_delete=models.SET_NULL, blank=True, null=True)
+
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, blank=True, null=True)
 
     attend_students = models.ManyToManyField(Student, null=True, blank=True)
 
